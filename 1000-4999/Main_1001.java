27baf88374d744cd013511c2cd72e6b93b8b7ddb
Hyun you jin <hyunyj21@gmail.com>
2020-07-14 23:08:58 +0900

    1001. A-B

---
+++ 1000-4999/Main_1001.java
@@ -0,0 +1,18 @@
+/*
+ * 1001. A-B
+ * https://www.acmicpc.net/problem/1000
+ */
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main_1001 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		int A = in.read() - '0';
+		in.read();
+		int B = in.read() - '0';
+		
+		System.out.println(A-B);
+	}
+}
