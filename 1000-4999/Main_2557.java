879d02654d78111abc1fac686b6da6964163b990
Hyun you jin <hyunyj21@gmail.com>
2020-07-16 22:47:35 +0900

    2557. Hello World

---
+++ 1000-4999/Main_2557.java
@@ -0,0 +1,10 @@
+/*
+ * 2557. Hello World
+ * https://www.acmicpc.net/problem/2557
+ */
+
+public class Main_2557 {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
