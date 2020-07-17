6e415e47a163d2bb7a2d9c60ec6cffaf810b74ee
Hyun you jin <hyunyj21@gmail.com>
2020-07-17 20:19:07 +0900

    2558. A+B - 2

---
+++ 1000-4999/Main_2558.java
@@ -0,0 +1,17 @@
+/*
+ * 2558. A+B - 2
+ * https://www.acmicpc.net/problem/2558
+ */
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main_2558 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		int A = Integer.parseInt(in.readLine());
+		int B = Integer.parseInt(in.readLine());
+		
+		System.out.println(A+B);
+	}
+}
