b72eb56a7f584517686911c16b7d15a93409b3e2
Hyun you jin <hyunyj21@gmail.com>
2020-08-08 23:14:43 +0900

    10926. ??!

---
+++ 10000-14999/Main_10926.java
@@ -0,0 +1,16 @@
+/*
+ * 10926. ??!
+ * https://www.acmicpc.net/problem/10926
+ */
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main_10926 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		String id = in.readLine();
+		
+		System.out.println(id+"??!");
+	}
+}
