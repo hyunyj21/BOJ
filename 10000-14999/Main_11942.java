1910927b82aa654b16e72f02e1153d07e7fdef23
Hyun you jin <hyunyj21@gmail.com>
2020-08-24 22:52:15 +0900

    11942. 고려대는 사랑입니다

---
+++ 10000-14999/Main_11942.java
@@ -0,0 +1,10 @@
+/*
+ * 11942. 고려대는 사랑입니다
+ * https://www.acmicpc.net/problem/11942
+ */
+
+public class Main_11942 {
+	public static void main(String[] args) {
+		System.out.println("고려대학교");
+	}
+}
